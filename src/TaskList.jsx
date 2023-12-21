9fb1e642c9741b3331def225cb2ebc098f1c8542
raaj-oo7 <raj.makadia22@gmail.com>
2023-12-21 12:38:17 +0530

    create using reusable components

---
+++ src/TaskList.jsx
@@ -0,0 +1,14 @@
+// TaskList.js
+import React from 'react';
+import './App.css';
+import TaskItem from './TaskItem';
+
+const TaskList = ({ tasks, onEdit, onDelete }) => (
+  <div className='task-list'>
+    {tasks.map((task, index) => (
+      <TaskItem key={index} task={task} onEdit={onEdit} onDelete={onDelete} />
+    ))}
+  </div>
+);
+
+export default TaskList;
