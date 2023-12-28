0446e7201f19cc6aeadf20349c304943b2a5f936
raaj-oo7 <raj.makadia22@gmail.com>
2023-12-28 17:41:33 +0530

    update the app and use reusable component and props

--- src/App.js
+++ src/App.js
@@ -1,138 +1,121 @@
 import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 import TaskList from './TaskList';
 import Modal from './Modal';
+import FormInput from './Modalform';
 
 function App() {
-  const [showAddModal, setShowAddModal] = useState(false);
-  const [showEditModal, setShowEditModal] = useState(false);
+  const [modalType, setModalType] = useState(null);
   const [taskData, setTaskData] = useState([]);
-  const [selectedTask, setSelectedTask] = useState({});
-  const [Title, setTitle] = useState('');
-  const [Date, setDate] = useState('');
-  const [Description, setDescription] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [formData, setFormData] = useState({
+    title: '',
+    date: '',
+    description: '',
+  });
 
+  useEffect(() => {
+    console.log('Fetch data from local storage...');
+    const storedTaskData = localStorage.getItem('taskData');
+    if (storedTaskData) {
+      console.log('Data found in local storage:', storedTaskData);
+      setTaskData(JSON.parse(storedTaskData));
+    }
+  }, []);
 
-  const handleAddButtonClick = () => {
-    setShowAddModal(true);
+  useEffect(() => {
+    localStorage.setItem('taskData', JSON.stringify(taskData));
+  }, [taskData]);
+
+  const openModal = (type, taskData) => {
+    setModalType(type);
+    resetForm();
+
+    if (type === 'edit' && taskData) {
+      setFormData({ ...taskData });
+    }
+  };
+
+  const closeModal = () => {
+    setModalType(null);
     resetForm();
   };
 
-  const handleCloseAddModal = () => {
-    setShowAddModal(false);
-    resetForm();
-  };
-
-  const handleCloseEditModal = () => {
-    setShowEditModal(false);
-    resetForm();
-  };
-
-  const handleOpenEditModal = (task) => {
-    setSelectedTask(task);
-    setTitle(task.title);
-    setDate(task.date);
-    setDescription(task.description);
-    setShowAddModal(false);
-    setShowEditModal(!showEditModal);
+  const handleFormChange = (e) => {
+    setFormData((prevData) => ({ ...prevData, [e.target.name]: e.target.value }));
   };
 
   const resetForm = () => {
-    setTitle('');
-    setDate('');
-    setDescription('');
+    setFormData({ title: '', date: '', description: '' });
     setErrorMessage('');
   };
 
   const handleAddNewTask = () => {
-    if (Title.trim() === '') {
+    if (formData.title.trim() === '') {
       setErrorMessage("Title can't be blank");
-    } else {
-      setErrorMessage('');
-      const newTask = {
-        title: Title,
-        date: Date,
-        description: Description,
-      };
-      setTaskData([...taskData, newTask]);
-      handleCloseAddModal();
+      return;
     }
+
+    setErrorMessage('');
+    const newTask = { ...formData, id: uuidv4() };
+    setTaskData([...taskData, newTask]);
+    closeModal();
   };
 
-  const handleEditTask = () => {
-    if (Title.trim() === '') {
-      setErrorMessage("Title can't be blank");
-    } else {
-      setErrorMessage('');
-      const updatedTaskData = taskData.map((task) =>
-        task === selectedTask
-          ? { ...task, title: Title, date: Date, description: Description }
-          : task
-      );
-      setTaskData(updatedTaskData);
-      handleCloseEditModal();
-    }
-  };
+  function handleEditTask(updatedTask) {
+    setTaskData((prevTaskData) => {
+      const updatedTaskData = [...prevTaskData];
+      const index = updatedTaskData.findIndex((task) => task.id === updatedTask.id);
+
+      if (index !== -1) {
+        updatedTaskData[index] = updatedTask;
+        return updatedTaskData;
+      }
+    });
+
+    closeModal();
+  }
 
   const handleDeleteTask = (taskToDelete) => {
     const updatedTaskData = taskData.filter((task) => task !== taskToDelete);
     setTaskData(updatedTaskData);
   };
 
-
-  useEffect(() => {
-    // Save data to localStorage
-    localStorage.setItem('taskData', JSON.stringify(taskData));
-  }, [taskData]);
-
-  useEffect(() => {
-    // Initialize app data from localStorage
-    const data = localStorage.getItem('taskData');
-    if (data) {
-      setTaskData(JSON.parse(data));
-    }
-  }, []);
-
   const modalForm = (
     <>
-      <div className="form-group">
-        <label htmlFor="formTitle" className='labelMessage'>Task Title</label>
-        <input
-          type="text"
-          className="form-control"
-          value={Title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <div className="error-message">{errorMessage}</div>
-      </div>
-      <div className="form-group">
-        <label htmlFor="formDate" className='labelMessage'>Due Date</label>
-        <input
-          type="date"
-          className="form-control"
-          value={Date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="formDescription" className='labelMessage'>Description</label>
-        <textarea
-          className="form-control"
-          rows="5"
-          value={Description}
-          onChange={(e) => setDescription(e.target.value)}
-        ></textarea>
-      </div>
+      <FormInput
+        label="Task Title"
+        type="text"
+        name="title"
+        value={formData.title}
+        onChange={handleFormChange}
+        errorMessage={errorMessage}
+      />
+
+      <FormInput
+        label="Due Date"
+        type="date"
+        name="date"
+        value={formData.date}
+        onChange={handleFormChange}
+      />
+
+      <FormInput
+        label="Description"
+        type="textarea"
+        name="description"
+        value={formData.description}
+        onChange={handleFormChange}
+      />
     </>
   );
-
   return (
     <div className="container">
       <h4 className="title">To-Do App 🎯</h4>
-      <button className="add-new-btn" onClick={handleAddButtonClick}>
+      <button type='button' className="add-new-btn" onClick={() => openModal('add')}>
         <span>Add New Task</span>
-        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="Add-new-icon">
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="add-new-icon">
           <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
           <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
           <g id="SVGRepo_iconCarrier">
@@ -149,26 +132,19 @@ function App() {
 
       <h5 className="task-title">Tasks</h5>
 
-      <TaskList tasks={taskData} onEdit={handleOpenEditModal} onDelete={handleDeleteTask} />
+      <TaskList tasks={taskData} onEdit={(task) => openModal('edit', task)} onDelete={handleDeleteTask} />
 
-      {showAddModal && (
+      {modalType && (
         <Modal
-          title="Add New Task"
-          onClose={handleCloseAddModal}
-          onConfirm={handleAddNewTask}
-        >
-          {modalForm}
-        </Modal>
-      )}
-
-      {showEditModal && (
-        <Modal
-          title="Edit Task"
-          onClose={handleCloseEditModal}
-          onConfirm={handleEditTask}
-        >
-          {modalForm}
-        </Modal>
+          formData={formData}
+          errorMessage={errorMessage}
+          handleFormChange={handleFormChange}
+          modalForm={modalForm}
+          modalType={modalType}
+          closeModal={closeModal}
+          handleAddNewTask={handleAddNewTask}
+          handleEditTask={handleEditTask}
+        />
       )}
     </div>
   );
